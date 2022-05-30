cb68b87622aed9008a0592d012247b748b7b6da0
Joel Brandao <joelsantosbrandao@gmail.com>
2022-05-30 23:04:55 +0100

    Springboot init WS_Utilizador

---
+++ WS_Utilizador/sprint/src/main/java/com/project/sprint/WsUtilizadorApplication.java
@@ -0,0 +1,13 @@
+package com.project.sprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsUtilizadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsUtilizadorApplication.class, args);
+	}
+
+}
