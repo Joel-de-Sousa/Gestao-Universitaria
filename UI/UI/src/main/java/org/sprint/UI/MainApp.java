764b8a6a3279aa495d7e1e674a3976f785a2e73d
Joel Brandao <joelsantosbrandao@gmail.com>
2022-06-03 01:21:28 +0100

    Continuação da implementação da UI atraves do design definido

--- UI/UI/src/main/java/org/sprint/UI/MainApp.java
+++ UI/UI/src/main/java/org/sprint/UI/MainApp.java
@@ -16,7 +16,8 @@ public class MainApp extends Application {
         
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
-        
+        root.setId("stack-pane");
+
         stage.setTitle("JavaFX and Maven");
         stage.setScene(scene);
         stage.show();
